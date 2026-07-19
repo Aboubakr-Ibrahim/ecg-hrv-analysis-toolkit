e26c9f0e85881a072fc00d5ed4f02e5ab7f606e5
Aboubakr-Ibrahim <abubakrh200@gmail.com>
2026-07-19 03:30:14 +0300

    build: add config/default_config.m

---
+++ config/default_config.m
@@ -0,0 +1,17 @@
+function cfg = default_config()
+%DEFAULT_CONFIG Reproducible defaults for ECG/HRV analysis.
+cfg.filterBandHz = [0.5 40];
+cfg.detectorBandHz = [5 20];
+cfg.filterOrder = 4;
+cfg.maxHeartRateBpm = 220;
+cfg.minHeartRateBpm = 30;
+cfg.peakThresholdScale = 0.45;
+cfg.refinementWindowSeconds = 0.08;
+cfg.nnRangeMs = [300 2000];
+cfg.localOutlierFraction = 0.20;
+cfg.frequencyResampleHz = 4;
+cfg.lfBandHz = [0.04 0.15];
+cfg.hfBandHz = [0.15 0.40];
+cfg.triangularBinWidthMs = 7.8125;
+cfg.saveFigures = true;
+end
