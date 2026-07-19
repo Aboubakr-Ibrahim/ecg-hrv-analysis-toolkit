15c37adaa3a5d50b14b63363903e66d56199b335
Aboubakr-Ibrahim <abubakrh200@gmail.com>
2026-07-19 03:31:29 +0300

    build: add src/run_hrv_pipeline.m

---
+++ src/run_hrv_pipeline.m
@@ -0,0 +1,26 @@
+function result = run_hrv_pipeline(ecg, fs, cfg, recordId, outputDir)
+%RUN_HRV_PIPELINE Analyze one ECG vector from signal to HRV report.
+arguments
+    ecg {mustBeNumeric,mustBeVector}
+    fs (1,1) double {mustBePositive}
+    cfg (1,1) struct
+    recordId (1,1) string = "record"
+    outputDir (1,1) string = ""
+end
+signal=preprocess_ecg(ecg,fs,cfg);
+[rPeaks,quality]=detect_r_peaks(signal.filtered,fs,cfg);
+[nnMs,nnTimeSec,cleaning]=clean_nn_intervals(rPeaks,fs,cfg);
+metrics=compute_time_domain_hrv(nnMs,cfg);
+frequency=compute_frequency_domain_hrv(nnMs,nnTimeSec,cfg);
+poincare=compute_poincare_hrv(nnMs);
+metrics=cell2struct([struct2cell(metrics);struct2cell(frequency);...
+    struct2cell(poincare)],[fieldnames(metrics);fieldnames(frequency);...
+    fieldnames(poincare)],1);
+result=struct('recordId',recordId,'fs',fs,'signal',signal,'rPeaks',rPeaks,...
+    'nnMs',nnMs,'nnTimeSec',nnTimeSec,'metrics',metrics,...
+    'quality',quality,'cleaning',cleaning);
+if strlength(outputDir)>0 && cfg.saveFigures
+    if ~isfolder(outputDir), mkdir(outputDir); end
+    create_hrv_figures(result,outputDir);
+end
+end
